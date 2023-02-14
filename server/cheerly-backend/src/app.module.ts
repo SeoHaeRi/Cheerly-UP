d72bea8f9890e631fc64ef1192f402364b865e49
HaeRi Seo <jackie100425@naver.com>
2023-02-14 16:07:46 +0900

    feat: 환경변수

--- server/cheerly-backend/src/app.module.ts
+++ server/cheerly-backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
 })
 export class AppModule {}
---
+++ server/cheerly-backend/src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
