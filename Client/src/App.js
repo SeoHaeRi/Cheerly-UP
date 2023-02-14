f9ffafbfa07d4a9e48924e498b4de1f8d670ab8e
omer_in <107422716+ome-r@users.noreply.github.com>
2023-02-14 15:50:16 +0900

    feat: route1

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,18 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Main from "./components/Main";
-import Group from "./components/Group";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Group from "./Pages/Group";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
-
-
+import Main from "./Pages/Main";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
-      {/* <Main /> */}
-      <Signin/>
-
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/group" element={<Group />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
