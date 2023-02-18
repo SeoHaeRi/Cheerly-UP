6870a8be05487c21e45e62c394dba3a688da3e8b
HaeRi Seo <jackie100425@naver.com>
2023-02-19 02:09:15 +0900

    feat: jwt

---
+++ server/cheerly-backend/src/domain/user/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/entities/User.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    // 부모 컴포넌트 부분 사용하기 위해서
+    super({
+      secretOrKey: 'SecretCheer123',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload) {
+    const { id } = payload;
+    const user: User = await this.userRepository.findOneBy({
+      id: id,
+    });
+    console.log('userVal : ', user);
+
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
--- server/cheerly-backend/src/domain/user/user.service.ts
+++ server/cheerly-backend/src/domain/user/user.service.ts
@@ -3,24 +3,32 @@ import { CreateUserDto } from './dto/CreateUser.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
 import { LoginUserDto } from './dto/LoginUser.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(userData: CreateUserDto): Promise<void> {
     return this.userRepository.createUser(userData);
   }
 
-  async logIn(userData: LoginUserDto): Promise<string> {
+  async logIn(userData: LoginUserDto): Promise<{ accessToken: string }> {
     const { id, pw } = userData;
-    const user = await this.userRepository.find({
-      where: { id: id },
+    const user = await this.userRepository.findOneBy({
+      id: id,
     });
-    console.log('user: ', user);
+    console.log('user -login: ', user);
 
-    if (user && (await bcrypt.compare(pw, user[0].pw))) {
-      return 'login success';
+    if (user && (await bcrypt.compare(pw, user.pw))) {
+      // 유저 토큰 생성 (secret + payload)
+      const payload = { id };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
