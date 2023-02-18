1d5a63e1d008aa547161de0f49df76d6b98d44f5
JsomeDev <wnthal1211@naver.com>
2023-02-18 12:59:41 +0900

    feat: 리덕스 일단 구조만

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Group from './Pages/Group';
 /*
@@ -19,26 +20,46 @@ import Study from './Pages/Study';
 import Board from './Pages/Board';
 import Navbar from './components/Navbar';
 import MyPage from './Pages/MyPage';
-
+import Error from './Pages/Error';
+import { useState } from 'react';
+import { setUserInfo } from './store/module/user';
 
 function App() {
+  const [init, setInit] = useState(true);
+
+  const dispatch = useDispatch();
+
+  const userInfo = {
+    id: sessionStorage.getItem('id'),
+    nickname: sessionStorage.getItem('nickname'),
+  };
+
+  if (userInfo.id) {
+    dispatch(setUserInfo(userInfo, true));
+  } else {
+    dispatch(setUserInfo(userInfo, false));
+  }
+
   return (
     <>
-      {/* <BrowserRouter>
-      </BrowserRouter> */}
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/board" element={<Board />} />
-          <Route path="/studygroup" element={<Group />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/study" element={<Study />} />
-          {/* 추후 에러 페이지 추가  + 마이페이지 추가, domain 은 수정 가능!*/}
-        </Routes>
-      </BrowserRouter>
-   
+      {init ? (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/studygroup" element={<Group />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/study" element={<Study />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/*" element={<Error />} />
+            {/* 추후 에러 페이지 추가  + 마이페이지 추가, domain 은 수정 가능!*/}
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        'Initializing ...'
+      )}
     </>
   );
 }
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './store';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+
+const reduxDevTool =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, reduxDevTool);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
 );
-
-reportWebVitals();
