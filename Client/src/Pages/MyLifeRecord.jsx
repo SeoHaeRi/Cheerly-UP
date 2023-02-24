75ac6ec83c3c21b9b825f616cbc3a789a32cc20d
omer_in <107422716+ome-r@users.noreply.github.com>
2023-02-24 18:04:52 +0900

    feat : 기록 페이지 수정 포스트잇 애니메이션 구현

--- Client/src/Pages/MyLifeRecord.jsx
+++ Client/src/Pages/MyLifeRecord.jsx
@@ -9,6 +9,88 @@ import moment from 'moment';
 import 'moment/locale/ko';
 import { jwtUtils } from '../utils/jwtUtils';
 import { useSelector } from 'react-redux';
+import goal from '../assets/goal.svg';
+import heart from '../assets/heart.svg';
+import yoga from '../assets/yoga.svg';
+import heart2 from '../assets/heart2.svg';
+import '../static/Peelred.scss';
+
+const Container = styled.div`
+  font-family: 'Jua', sans-serif;
+  padding: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 300px);
+  -ms-grid-rows: auto;
+  grid-template-rows: 50px;
+  grid-auto-rows: 100px;
+  grid-gap: 30px 10px;
+  justify-content: center;
+  background: #ffffff;
+  box-sizing: border-box;
+
+  p {
+    font-size: 1.25rem;
+    text-align: center;
+    position: relative;
+    top: 20px;
+    color: #ffeb3b;
+    /* background-color: #b0b0fe; */
+  }
+
+  .life {
+    font-size: 1rem;
+    text-align: center;
+    align-items: center;
+    position: relative;
+    /* width: 100%; */
+    padding: 10px;
+    /* top: 20px; */
+    color: #3f51b5;
+    background-color: #ffeb3b;
+  }
+  .contents {
+    font-size: 1.2rem;
+    text-align: center;
+    align-items: center;
+    position: relative;
+    top: 30px;
+    color: #ffeb3b;
+    /* background-color: white; */
+    border-radius: 5px;
+  }
+`;
+
+const Treeimg = styled.img`
+  /* z-index: -1;
+  width: 100%;
+  height: 80%;
+  position: absolute; */
+  z-index: -1;
+  width: 90%;
+  left: -40rem;
+  right: rem;
+  /* height: 80%; */
+  position: absolute;
+`;
+
+const MainHeader = styled.div`
+  font-family: 'Jua', sans-serif;
+  background-color: #e91e63;
+  width: 100%;
+  margin-top: 30px;
+  padding: 20px;
+  color: #ffffff;
+  font-size: 1.75rem;
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 80%;
+  height: 80%;
+  position: relative;
+  margin: 0 auto;
+  left: 30px;
+`;
 
 function MyLifeRecord() {
   const userID = useSelector((state) => state.user.user.data.user_id);
@@ -50,14 +132,18 @@ function MyLifeRecord() {
   return (
     <>
       <MainHeader>내가 성취한 라이프 기록</MainHeader>
+      <Treeimg src={heart2} alt="heart" />
+
       <Container>
         {lifeRecord.map((life, index) => (
-          <div className="card-wrapper" key={index}>
-            <div className="card-body-text">
-              성취한 날짜
-              <div className="date">{life.date}</div>
-              <div className="card-body-text-content">🏃{life.content}</div>
-            </div>
+          <div className="pad-red" key={index}>
+            <p>🐥오늘도 Level UP🐥</p> <br />
+            <p className="life">DATE : {life.date}</p>
+            <p className="contents">🏃{life.content}</p>
+            <Img src={yoga} alt="goal" />
+            <span className="peel-red">
+              <span className="peel-red-back"></span>
+            </span>
           </div>
         ))}
       </Container>
@@ -65,25 +151,3 @@ function MyLifeRecord() {
   );
 }
 export default MyLifeRecord;
-const Container = styled.div`
-  padding: 0;
-  display: grid;
-  grid-template-columns: repeat(3, 300px);
-  -ms-grid-rows: auto;
-  grid-template-rows: 50px;
-  grid-auto-rows: 100px;
-  grid-gap: 30px 10px;
-  justify-content: center;
-  background: white;
-  box-sizing: border-box;
-`;
-
-const MainHeader = styled.div`
-  background-color: #1363df;
-  width: 100%;
-  margin-top: 30px;
-  padding: 20px;
-  color: white;
-  font-size: 1.75rem;
-  text-align: center;
-`;
--- Client/src/components/Typing.jsx
+++ Client/src/components/Typing.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Textdiv = styled.div`
-  font-size: 2.5rem;
+  font-family: 'Jua', sans-serif;
+  font-size: 2.25rem;
   color: #2196f3;
   text-align: center;
   width: 100%;
@@ -11,10 +12,37 @@ const Textdiv = styled.div`
   top: 25rem;
   left: 22%;
   font-weight: 800;
+
+  .text {
+    @media screen and (max-width: 1210px) {
+      font-size: 2.25rem;
+
+      position: absolute;
+      text-align: center;
+      top: 25rem;
+      left: 22%;
+      font-weight: 800;
+    }
+    @media screen and (max-width: 768px) {
+      font-size: 1.25rem;
+
+      position: absolute;
+      text-align: center;
+      top: 25rem;
+      left: 22%;
+      font-weight: 800;
+      /* position: absolute;
+    margin: 0;
+    padding: 0;
+    left: 23%;
+    top: 18rem;
+    max-width: 20rem; */
+    }
+  }
 `;
 
 export default function Typing() {
-  const txt = '나의 오늘 기록이 궁금하다면?';
+  const txt = '나의 데일리 기록이 궁금하다면?';
   const [text, setText] = useState('');
   const [count, setCount] = useState(0);
   const [Color, setColor] = useState('black');
