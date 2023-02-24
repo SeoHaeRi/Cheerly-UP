56cdcbaa2704a2f5704d5f753d7ab7f55e171cb0
omer_in <107422716+ome-r@users.noreply.github.com>
2023-02-24 18:24:20 +0900

    feat: life, study 오른쪽 반응형

--- Client/src/components/Typing.jsx
+++ Client/src/components/Typing.jsx
@@ -1,6 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+const Textp = styled.p`
+  font-family: 'Jua', sans-serif;
+  font-size: 2.25rem;
+  color: #2196f3;
+  text-align: center;
+  width: 100%;
+  height: 10rem;
+  position: relative;
+  top: 25rem;
+  left: 22%;
+  font-weight: 800;
+
+  @media screen and (max-width: 1700px) {
+    font-size: 2.25rem;
+
+    position: relative;
+    text-align: center;
+    top: 25rem;
+    left: 16rem;
+    font-weight: 800;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 1.25rem;
+
+    position: relative;
+    text-align: center;
+    top: 25rem;
+    left: 10rem;
+    font-weight: 800;
+    /* position: absolute;
+    margin: 0;
+    padding: 0;
+    left: 23%;
+    top: 18rem;
+    max-width: 20rem; */
+  }
+`;
 const Textdiv = styled.div`
   font-family: 'Jua', sans-serif;
   font-size: 2.25rem;
@@ -14,7 +51,7 @@ const Textdiv = styled.div`
   font-weight: 800;
 
   .text {
-    @media screen and (max-width: 1210px) {
+    @media screen and (max-width: 1250px) {
       font-size: 2.25rem;
 
       position: absolute;
@@ -63,11 +100,11 @@ export default function Typing() {
   });
   return (
     <>
-      <Textdiv>
-        <p className="text" style={{ color: Color }}>
-          {text}
-        </p>
-      </Textdiv>
+      {/* <Textdiv> */}
+      <Textp className="text" style={{ color: Color }}>
+        {text}
+      </Textp>
+      {/* </Textdiv> */}
     </>
   );
 }
