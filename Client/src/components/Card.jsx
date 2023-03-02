3be14e2229879b06b9ad805b44c0640ed1aab282
seohags <048965s@naver.com>
2023-03-02 22:48:45 +0900

    delete files

--- Client/src/components/Card.jsx
+++ Client/src/components/Card.jsx
@@ -1,15 +1,15 @@
-import "../static/Card.css";
-import {useNavigate} from "react-router-dom";
+import '../static/Card.css';
+import { useNavigate } from 'react-router-dom';
 
-export const Card = ({board_id, title, content, img_url, username, date}) => {
+export const Card = ({ board_id, title, content, username, date }) => {
   const navigate = useNavigate();
   return (
-    <div className="card-wrapper" onClick={() => {
-      navigate(`/board/${board_id}`)
-    }}>
-      <div className="card-body-img">
-        {/* <img src={img_url}/> */}
-      </div>
+    <div
+      className="card-wrapper"
+      onClick={() => {
+        navigate(`/board/${board_id}`);
+      }}
+    >
       <div className="card-body-text">
         <div className="card-body-text-title">{title}</div>
         <div className="card-body-text-content">{content}</div>
